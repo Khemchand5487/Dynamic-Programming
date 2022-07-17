f96dd41af230778c9e92c4c0396de6d87d020e11
Khem Chand <khemchand5487@gmail.com>
2022-07-17 23:35:22 +0530

    Create Q1.cpp

---
+++ Q1.cpp
@@ -0,0 +1,71 @@
+/** Solution By khemchand5487 **/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(v) v.begin(), v.end()
+#define debug(x) cout << #x <<" "; _print(x); cout << endl;
+
+typedef long long ll;
+
+//for debugging
+template <class T>
+void _print(T t) {cout << t;}
+
+template <class T, class V> void _print(pair <T, V> p) {cout << "{"; _print(p.first); cout << ","; _print(p.second); cout << "}" << endl;}
+template <class T> void _print(vector <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]" << endl;}
+template <class T> void _print(set <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]" << endl;}
+template <class T> void _print(multiset <T> v) {cout << "[ "; for (T i : v) {_print(i); cout << " ";} cout << "]" << endl;}
+template <class T, class V> void _print(map <T, V> v) {cout << "[ "; for (auto i : v) {_print(i); cout << " ";} cout << "]" << endl;}
+template <class T, class V> void _print(unordered_map <T, V> v) {cout << "[ "; for (auto i : v) {_print(i); cout << " ";} cout << "]" << endl;}
+//END
+
+//reading input 
+template<typename... T>
+void input(T&... args) {(((cin>>args), ...));}
+
+//writing output
+template<typename... T>
+void output(T... args) {(((cout<<args<<" "), ...));  cout<<endl;}
+
+// for finding mod with 10^9 + 7
+int mod(int a)
+{
+    int m = 1000000007;
+    return (a % m + m) % m;
+}
+
+////////solution goes here////////
+void solve()
+{
+    int n;
+    cin>>n;
+    int dp[n+1], sum=0;
+    dp[0] = 1;
+
+    for(int i=1; i<=n;i++) {
+        sum=0;
+        for(int j=1;j<=6;j++) {
+            if(i-j>=0) sum = mod(sum+dp[i-j]);
+        }
+        dp[i] = sum;
+    }
+    
+    cout<<dp[n]<<endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int t = 1;
+    // cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+/** Solution By khemchand5487 **/
